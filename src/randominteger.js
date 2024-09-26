83779871932278691e58eb2ea615e90e904e8dd7
RUSEN <125656846+RBDSQS@users.noreply.github.com>
2024-09-26 16:49:35 +0800

    Delete src directory

--- src/randominteger.js
+++
@@ -1,8 +0,0 @@
-function randomInt() {
-    // 生成1到10之间的随机整数
-    return Math.floor(Math.random() * 100) + 1;
-}
-
-function randomRange(lower, upper) {
-    return Math.floor(Math.random() * (upper - lower + 1)) + lower;
-}
